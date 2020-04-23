99c2a99c45fae9cdac52cf8ea1917cf229663ed5
s.weise <s.weise@sw-pc>
2020-04-22 19:23:42 -0700

    kjnhbgvfcdx

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,6 +1,17 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	public static void main(String[] args) {
+		  new GuestBook().createUI();
+	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,5 +20,30 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+ JFrame frame = new JFrame();
+ JPanel panel = new JPanel();
+ JButton button = new JButton("Add Name");
+ JButton buttons = new JButton("View Names");
+
+   void createUI() {
+frame.add(panel);
+panel.add(button);
+panel.add(buttons);
+button.addActionListener(this);
+buttons.addActionListener(this);
+frame.pack();
+frame.setVisible(true);
+  
+   }
+
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	// TODO Auto-generated method stub
+	if (arg0.getSource()== button) {
+		String input = JOptionPane.showInputDialog("Enter any Name");
+	}
+	else {
+		JOptionPane.showMessageDialog(null,"");
+	}
 }
-	GUI();compost 
+}
